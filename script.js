363c8b3ac7aee85ff80aeb72264a7267db15704f
Jacob Rees <reescodes@gmail.com>
2023-07-09 14:43:30 -0600

    Add buttons and eventlisteners to buttons

--- script.js
+++ script.js
@@ -35,43 +35,78 @@ function playRound (playerSelection, computerSelection) {
     else {
         outcome = 'Computer Wins'
     }
+    console.log(`Player selected ${playerSelection}. Computer selected ${computerSelection}`)
     return outcome
     
 }
+const outcome = document.querySelector('.outcome');
+
+const rockButton = document.querySelector('.rock');
+rockButton.addEventListener('click', () => {
+    const playerSelection = 'rock';
+    const computerSelection = getComputerChoice();
+    const outcome = playRound(playerSelection, computerSelection);
+    console.log(outcome);
+
+});
+
+const paperButton = document.querySelector('.paper');
+paperButton.addEventListener('click', () => {
+    const playerSelection = 'paper';
+    const computerSelection = getComputerChoice();
+    const outcome = playRound(playerSelection, computerSelection);
+    console.log(outcome);
+
+});
+
+const scissorsButton = document.querySelector('.scissors');
+scissorsButton.addEventListener('click', () => {
+    const playerSelection = 'scissors';
+    const computerSelection = getComputerChoice();
+    const outcome = playRound(playerSelection, computerSelection);
+    console.log(outcome);
+
+});
+
+
+
+
+
+
+
+
 
  
 //main game loop plays game to 5 points
 
-function game() {
-    let player_score = 0
-    let computer_score = 0
-    while (player_score !== 5 && computer_score !== 5) {
-        let playerSelection = prompt().toLowerCase();
-        let computerSelection = getComputerChoice();
-        let outcome = playRound(playerSelection, computerSelection)
-        if (outcome === 'Player Wins'){
-            player_score ++
-        }
-        else if (outcome === "Tie") {
+// function game() {
+//     let player_score = 0
+//     let computer_score = 0
+//     while (player_score !== 5 && computer_score !== 5) {
+//         let playerSelection = prompt().toLowerCase();
+//         let computerSelection = getComputerChoice();
+//         let outcome = playRound(playerSelection, computerSelection)
+//         if (outcome === 'Player Wins'){
+//             player_score ++
+//         }
+//         else if (outcome === "Tie") {
 
-        }
-        else {
-            computer_score ++
-        }
-        console.log(`You chose ${playerSelection}. Computer chose ${computerSelection}. ${outcome}` )
-        console.log(`Player score: ${player_score} Computer score: ${computer_score}`)
+//         }
+//         else {
+//             computer_score ++
+//         }
+//         console.log(`You chose ${playerSelection}. Computer chose ${computerSelection}. ${outcome}` )
+//         console.log(`Player score: ${player_score} Computer score: ${computer_score}`)
   
 
-    }
-    if (player_score === 5) {
-        console.log('You win!!!')
-    }
-    else {
-        console.log('Computer wins! You lose.')
-    }
+//     }
+//     if (player_score === 5) {
+//         console.log('You win!!!')
+//     }
+//     else {
+//         console.log('Computer wins! You lose.')
+//     }
 
     
-}
+// }
 
-game()
-console.log('Testing this new branch!')
