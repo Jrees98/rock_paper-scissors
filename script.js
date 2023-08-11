4f37f0cb569d663c83a4a2a2879ed99c802245d4
Jacob Rees <reescodes@gmail.com>
2023-08-11 09:25:41 -0600

    Add playRound function and testing. Prefab to win.

--- script.js
+++ script.js
@@ -2,14 +2,33 @@ function getComputerChoice() {
     let choice = ''
     let number = Math.floor(Math.random() * 3) + 1;
     if (number === 1) {
-        choice = 'Rock';
+        choice = 'rock';
     } else if (number === 2) {
-        choice = 'Paper';
+        choice = 'paper';
     } else {
-        choice = 'Scissors';
+        choice = 'scissors';
     }
 
     return choice
 }
 
+function playRound(playerSelection, computerSelection) {
+    let result = ''
+    let prefab = `You win! You chose ${playerSelection} and the computer chose ${computerSelection}`
+    if (playerSelection === computerSelection) {
+        result = `You both selected ${playerSelection}`;
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+        result = prefab
+    } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+        result = prefab
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        result = prefab
+    } else {
+        result = `You lose... You chose ${playerSelection} and the computer chose ${computerSelection}`
+    }
+    return result
+}
 
+console.log(playRound('rock', getComputerChoice()))
+console.log(playRound('paper', getComputerChoice()))
+console.log(playRound('scissors', getComputerChoice()))
