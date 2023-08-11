3295d23b092335b557dc5ca1829d39666f4cdb19
Jacob Rees <reescodes@gmail.com>
2023-08-11 07:39:49 -0600

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-test
+function getComputerChoice() {
+    let choice = ''
+    let number = Math.floor(Math.random() * 3) + 1;
+    if (number === 1) {
+        choice = 'Rock';
+    } else if (number === 2) {
+        choice = 'Paper';
+    } else {
+        choice = 'Scissors';
+    }
+
+    return choice
+}
+
+
